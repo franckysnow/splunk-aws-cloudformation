188a27a660ffef4c602d6345071dd944099cd493
Franck Youssef <franck.youssef@bluewin.ch>
2017-11-22 22:21:43 +0100

    added master template

--- scripts/cfStackDeploy.sh
+++ scripts/cfStackDeploy.sh
@@ -8,7 +8,7 @@ if [[ -z "${AWSBUCKET}" ]]; then
     exit 1
 fi
 
-stackURL=https://s3-eu-west-1.amazonaws.com/$AWSBUCKET/$path/splunk_network.template.yml
+stackURL=https://s3-eu-west-1.amazonaws.com/$AWSBUCKET/$path/master.template.yml
 echo $stackURL
-aws cloudformation create-stack --stack-name Splunk-test --template-url $stackURL --parameters ParameterKey=SSHLocation,ParameterValue=77.30.42.142/32
+aws cloudformation create-stack --stack-name Splunk-test --template-url $stackURL --parameters ParameterKey=SSHFrom,ParameterValue=77.30.42.142/32 ParameterKey=BucketName,ParameterValue=franckysnow-utility
 
