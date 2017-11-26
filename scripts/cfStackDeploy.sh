d3f4be6c73ae7cb90897fd4d05bccdb325266896
Franck Youssef <franck.youssef@bluewin.ch>
2017-11-26 19:34:12 +0100

    Cleanup in bastion template

--- scripts/cfStackDeploy.sh
+++ scripts/cfStackDeploy.sh
@@ -1,18 +1,20 @@
 #! /bin/sh
 
-home=..
-path=splunk-deployment/templates
+bucketPath=splunk-deployment/templates
 
 if [[ -z "${AWSBUCKET}" ]]; then
     echo "AWSBUCKET variable is unset. set using \"export AWSBUCKET=my-dns-compliant-bucket\""
     exit 1
 fi
 
-stackURL=https://s3-eu-west-1.amazonaws.com/$AWSBUCKET/$path/master.template.yml
+# get my public ip address
+publicIpAddress=`curl -s https://api.ipify.org`
+
+stackURL=https://s3-eu-west-1.amazonaws.com/$AWSBUCKET/$bucketPath/master.template.yml
 echo $stackURL
 aws cloudformation create-stack --stack-name Splunk-test \
     --template-url $stackURL \
-    --parameters ParameterKey=SSHFrom,ParameterValue=77.30.42.142/32 \
+    --parameters ParameterKey=SSHFrom,ParameterValue=$publicIpAddress/32 \
                  ParameterKey=BucketName,ParameterValue=franckysnow-utility \
                  ParameterKey=KeyName,ParameterValue=Splunk-External
 
